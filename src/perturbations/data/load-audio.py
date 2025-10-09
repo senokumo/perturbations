4a86832c083ff3174e612081089d48dd414022a5
senokumo <senokumo@gmail.com>
2025-10-09 18:55:44 +0200

    add load-audio module

---
+++ src/perturbations/data/load-audio.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+import essentia.standard as es
+
+ROOT = Path(__file__).resolve().parent.parent.parent.parent
+AUDIO_DIR = ROOT / 'audio'
+SONG1_DIR = AUDIO_DIR / 'ween_rosesarefree' / 'raw'
+
+
+def load_from_directory(audio_directory, loader_function):
+  audio_files = {}
+  for file_path in audio_directory.glob("*.flac"):
+    audio = loader_function(file_path)
+    audio_files[file_path.name] = audio
+
+  return audio_files
+
+
+def load_audio(audio_path):
+  try:
+    audio = es.MonoLoader(filename=str(audio_path))()
+    return audio
+  except Exception as e:
+    print(f"failed to load audio from {audio_path}: {e}")
+    return None
+
+def load_metadata(audio_path):
+  try:
+    metadata = es.MetadataReader(filename=str(audio_path))()
+    return metadata
+  except Exception as e:
+    print(f"failed to load metadata from {audio_path}: {e}")
+    return None
+
+
+metadata = load_from_directory(SONG1_DIR, load_metadata)
+
+print(metadata)

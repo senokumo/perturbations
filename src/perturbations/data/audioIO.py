3e121a9919e5f95883a5c20aea405d7a5c10f817
senokumo <senokumo@gmail.com>
2025-10-09 20:20:44 +0200

    name change audioIO

--- src/perturbations/data/audioIO.py
+++ src/perturbations/data/audioIO.py
@@ -1,3 +1,4 @@
+
 from pathlib import Path
 import essentia.standard as es
 
@@ -5,10 +6,12 @@ ROOT = Path(__file__).resolve().parent.parent.parent.parent
 AUDIO_DIR = ROOT / 'audio'
 SONG1_DIR = AUDIO_DIR / 'ween_rosesarefree' / 'raw'
 
+FILE_FORMAT = '*.flac'
+SAMPLE_RATE = 22050
 
 def load_from_directory(audio_directory, loader_function):
   audio_files = {}
-  for file_path in audio_directory.glob("*.flac"):
+  for file_path in audio_directory.glob(FILE_FORMAT):
     audio = loader_function(file_path)
     audio_files[file_path.name] = audio
 
@@ -17,11 +20,12 @@ def load_from_directory(audio_directory, loader_function):
 
 def load_audio(audio_path):
   try:
-    audio = es.MonoLoader(filename=str(audio_path))()
+    audio = es.MonoLoader(filename=str(audio_path), sampleRate=SAMPLE_RATE)()
     return audio
   except Exception as e:
     print(f"failed to load audio from {audio_path}: {e}")
     return None
+
 
 def load_metadata(audio_path):
   try:
@@ -31,3 +35,8 @@ def load_metadata(audio_path):
     print(f"failed to load metadata from {audio_path}: {e}")
     return None
 
+
+audio = load_from_directory(SONG1_DIR, load_audio)
+
+
+print(dir(audio))

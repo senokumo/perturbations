b1542accbd5470d4f12263f20edd7525b3388b67
senokumo <senokumo@gmail.com>
2025-10-11 14:44:33 +0200

    add consts file

---
+++ src/perturbations/consts.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+
+# constants
+
+## audio extraction config
+FILE_FORMAT = '*.flac'
+SAMPLE_RATE = 22050
+
+## song names
+W_RAF = 'ween_rosesarefree'
+
+
+
+# dataclasses
+
+@dataclass(frozen=True)
+class Directory:
+  ROOT = Path(__file__).resolve() # .parent.parent.parent
+  AUDIO_DIR = ROOT / 'audio'
+
+  @staticmethod
+  def song_dir(performance):
+      return Directory.AUDIO_DIR / performance / 'raw'
+
+
+# test
+perf_dir = Directory.AUDIO_DIR
+print(perf_dir)

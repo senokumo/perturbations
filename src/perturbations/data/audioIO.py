700f501e7802644ef4867539e519b992df7a14e3
senokumo <senokumo@gmail.com>
2025-10-11 14:45:19 +0200

    format audioIO

--- src/perturbations/data/audioIO.py
+++ src/perturbations/data/audioIO.py
@@ -1,27 +1,31 @@
 
 from pathlib import Path
 import essentia.standard as es
+from perturbations.consts import W_RAF, FILE_FORMAT, SAMPLE_RATE, Directory
 
-ROOT = Path(__file__).resolve().parent.parent.parent.parent
-AUDIO_DIR = ROOT / 'audio'
-SONG1_DIR = AUDIO_DIR / 'ween_rosesarefree' / 'raw'
 
-FILE_FORMAT = '*.flac'
-SAMPLE_RATE = 22050
+# uses essentia for audio io
+# TODO compare essentia with librosa in terms of speed
 
-def load_from_directory(audio_directory, loader_function):
-  audio_files = {}
+
+
+def load_performances(audio_directory, loader_function):
+  performances = {}
   for file_path in audio_directory.glob(FILE_FORMAT):
     audio = loader_function(file_path)
-    audio_files[file_path.name] = audio
+    performances[file_path.name] = audio
 
-  return audio_files
+  return performances
 
 
 def load_audio(audio_path):
   try:
-    audio = es.MonoLoader(filename=str(audio_path), sampleRate=SAMPLE_RATE)()
+    audio = es.MonoLoader(
+      filename=str(audio_path), 
+      sampleRate=SAMPLE_RATE
+    )()
     return audio
+  
   except Exception as e:
     print(f"failed to load audio from {audio_path}: {e}")
     return None
@@ -29,14 +33,12 @@ def load_audio(audio_path):
 
 def load_metadata(audio_path):
   try:
-    metadata = es.MetadataReader(filename=str(audio_path))()
+    metadata = es.MetadataReader(
+      filename=str(audio_path)
+    )()
     return metadata
   except Exception as e:
     print(f"failed to load metadata from {audio_path}: {e}")
     return None
 
 
-audio = load_from_directory(SONG1_DIR, load_audio)
-
-
-print(dir(audio))
